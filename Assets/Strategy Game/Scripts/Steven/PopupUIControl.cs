35dbe1ed93f31c68c0de58aabb1643293b504d80
SteveoBurns <steveo.burns.777@gmail.com>
2021-09-15 17:16:05 +1000

    Update to popup control

--- Assets/Strategy Game/Scripts/Steven/PopupUIControl.cs
+++ Assets/Strategy Game/Scripts/Steven/PopupUIControl.cs
@@ -1,29 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
 using TMPro;
-
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace StrategyGame.UI
 {
+    /// <summary>
+    /// Handles the enabling of the UI popup for user feedback and information.
+    /// </summary>
     public class PopupUIControl : MonoBehaviour
     {
+        /// <summary>
+        /// Struct for the popup display data
+        /// </summary>
         [Serializable]
         public struct PopUpStruct
         {
             public string title;
             [TextArea] public string description;
         }
+        
         [Header("Popup Panel Elements")]
         [SerializeField] private GameObject popupPanel;
         [SerializeField] private TMP_Text title;
         [SerializeField] private TMP_Text description;
         [SerializeField] private Button startButton;
         [SerializeField] private Button retryButton;
-        // maybe a start button for begining of the game instructions?
+        
     
         [Header("Messages")]
         [SerializeField] private PopUpStruct winPopup;
@@ -43,12 +48,19 @@ namespace StrategyGame.UI
             retryButton.enabled = false;
         }
 
+        /// <summary>
+        /// Function for the start game button on the popup UI panel
+        /// </summary>
         public void StartButton()
         {
             popupPanel.SetActive(false);
             Time.timeScale = 1;
         }
-
+        
+        /// <summary>
+        /// Shows the popup on the cnavas with the passed data and pauses the timescale.
+        /// </summary>
+        /// <param name="_popUpStruct">The struct values to be displayed</param>
         public void ShowPopup(PopUpStruct _popUpStruct)
         {
             // Set the popup active and set the fields
