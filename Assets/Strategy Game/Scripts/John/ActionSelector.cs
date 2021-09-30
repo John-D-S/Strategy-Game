5481249274ccc946bc224377140fc5cae605b470
John <68535837+John-D-S@users.noreply.github.com>
2021-09-30 23:49:53 +1000

    Added Item Script, worked on action selector UI and the player controller action system

--- Assets/Strategy Game/Scripts/John/ActionSelector.cs
+++ Assets/Strategy Game/Scripts/John/ActionSelector.cs
@@ -1,11 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using TMPro;
+
 using UnityEngine;
 using UnityEngine.UI;
 
+using Button = UnityEngine.UIElements.Button;
+
+[RequireComponent(typeof(RectTransform))]
 public class ActionSelector : MonoBehaviour
 {
-    [SerializeField] private RectTransform buttonPanel;
-    public Button actionButtonPrefab;
+    private RectTransform rectTransform;
     
+    [SerializeField] private RectTransform buttonPanel;
+    private PlayerController player;
+    public GameObject ActionButtonPrefab;
+    private List<PlayerAction> availablePlayerActions = new List<PlayerAction>();
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        rectTransform = GetComponent<RectTransform>();
+        gameObject.SetActive(false);
+    }
+    
+    private void OnEnable()
+    {
+        rectTransform.anchoredPosition = Input.mousePosition;
+        GameObject instantiatedButtonObject = Instantiate(ActionButtonPrefab, buttonPanel);
+        Button instantiatedButton = instantiatedButtonObject.GetComponent<Button>();
+        TextMeshProUGUI buttonText = instantiatedButtonObject.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void OnDisable()
+    {
+        foreach(RectTransform childTransform in buttonPanel)
+        {
+            Destroy(childTransform);
+        }
+    }
 }
--- Assets/Strategy Game/Scripts/John/PlayerController.cs
+++ Assets/Strategy Game/Scripts/John/PlayerController.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 using UnityEditor.Experimental.GraphView;
 
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 using UnityEngine.WSA;
 
+using Cursor = UnityEngine.Cursor;
 using Object = UnityEngine.Object;
 
-public abstract class Action
+public abstract class PlayerAction
 {
-    public Action(NavGridNode _node, PlayerController _player)
+    public PlayerAction(NavGridNode _node, PlayerController _player)
     {
         node = _node;
         player = _player;
@@ -44,7 +47,7 @@ public abstract class Action
     protected abstract void UndoAction();
 }
 
-public class Move : Action
+public class Move : PlayerAction
 {
     private NavGridNode lastNode;
     
@@ -62,7 +65,7 @@ public class Move : Action
     }
 }
 
-public class PlaceGameObject : Action
+public class PlaceGameObject : PlayerAction
 {
     private GameObject objectToPlace;
     private GameObject placedObject;
@@ -87,22 +90,61 @@ public class PlaceGameObject : Action
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int actionsAtStartOfTurn = 5;
-    private List<Action> availableActions = new List<Action>();
+    private List<PlayerAction> availableActions = new List<PlayerAction>();
     
     private NavGridAgent agent;
     public NavGridAgent PlayerAgent => agent;
     public NavGridNode CurrentNode => agent.CurrentNode;
     public List<NavGridNode> NeighboringNodes => CurrentNode.Neighbors;
+    public List<Item> collectedItems = new List<Item>();
+    public List<PlayerAction> actionsDoneThisTurn;
+
+    private NavGridNode selectedNode;
+    public NavGridNode SelectedNode => selectedNode;
+
+    public Dictionary<GameObject, NavGridNode> NeighboringNodesByGameObject
+    {
+        get
+        {
+            Dictionary<GameObject, NavGridNode> returnValue = new Dictionary<GameObject, NavGridNode>();
+            foreach(NavGridNode node in NeighboringNodes)
+            {
+                returnValue[node.gameObject] = node;
+            }
+
+            return returnValue;
+        }
+    }
+    private ActionSelector actionSelector;
+    
+    public List<PlayerAction> AvailableActions(NavGridNode _node)
+    {
+        List<PlayerAction> returnValue = new List<PlayerAction>();
+        returnValue.Add(new Move(_node, this));
+        foreach(Item collectedItem in collectedItems)
+        {
+            returnValue.Add(new PlaceGameObject(_node, this, collectedItem.gameObject));
+        }
+        return returnValue;
+    }
+
+    public void TryPickupItem()
+    {
+        Item item = Item.ItemNearPosition(agent.AgentNavGrid.GridSize * 0.5f, transform.position);
+        if(item)
+        {
+            collectedItems.Add(item);
+            item.gameObject.SetActive(false);
+        }
+    }
     
     private TurnManager turnManager;
 
-    public List<Action> actionsDoneThisTurn;
-    
-    public void DoAction(NavGridNode _node, Action _action)
+    public void DoAction(NavGridNode _node, PlayerAction _playerAction)
     {
         if(actionsDoneThisTurn.Count < actionsAtStartOfTurn)
         {
-            actionsDoneThisTurn.Add(_action);
+            actionsDoneThisTurn.Add(_playerAction);
             actionsDoneThisTurn[actionsDoneThisTurn.Count - 1].PlayerExecuteAction();
         }
     }
@@ -111,16 +153,10 @@ public class PlayerController : MonoBehaviour
     {
         if(actionsDoneThisTurn.Count > 0)
         {
-            Action lastAction = actionsDoneThisTurn[actionsDoneThisTurn.Count];
-            lastAction.PlayerUndoAction();
-            actionsDoneThisTurn.Remove(lastAction);
+            PlayerAction lastPlayerAction = actionsDoneThisTurn[actionsDoneThisTurn.Count];
+            lastPlayerAction.PlayerUndoAction();
+            actionsDoneThisTurn.Remove(lastPlayerAction);
         }
-    }
-
-
-    public void StartNewTurn()
-    {
-        actionsRemaining = actionsAtStartOfTurn;
     }
 
     public void EndTurn()
@@ -130,13 +166,37 @@ public class PlayerController : MonoBehaviour
 
     private void UpdatePlayerActions()
     {
-        
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            NavGridNode node = null;
+            if(Physics.Raycast(mouseRay, out hit))
+            {
+                Dictionary<GameObject, NavGridNode> currentNeighborNodesbyGo = NeighboringNodesByGameObject;
+                GameObject hitObject = hit.collider.gameObject;
+                if(currentNeighborNodesbyGo.ContainsKey(hitObject))
+                {
+                    node = currentNeighborNodesbyGo[hitObject];
+                }
+            }
+            if(node && NeighboringNodes.Contains(node))
+            {
+                selectedNode = node;
+                actionSelector.gameObject.SetActive(true);
+            }
+            else
+            {
+                actionSelector.gameObject.SetActive(false);
+            }
+        }
     }
     
     private void Start()
     {
         turnManager = TurnManager.theTurnManager;
         agent = GetComponent<NavGridAgent>();
+        actionSelector = FindObjectOfType<ActionSelector>(true);
     }
     
     private void Update()
